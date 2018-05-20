d8a0a1b8452a3c8cdc04bd8f4993af606ca8629d
nyanpasu64 <nyanpasu64@tuta.io>
2018-05-20 04:18:42 -0700

    wat

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='amk_tools',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='nyanpasu64',
+    author_email='',
+    description=''
+)
