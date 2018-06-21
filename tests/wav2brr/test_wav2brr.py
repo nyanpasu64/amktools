16201add33fd3f382b83089aab84eb5f88eea7de
nyanpasu64 <nyanpasu64@tuta.io>
2018-06-20 23:18:33 -0700

    Reorder test_wav2brr unit tests

--- tests/wav2brr/test_wav2brr.py
+++ tests/wav2brr/test_wav2brr.py
@@ -56,12 +56,55 @@ def test_pushd():
             assert root / SUB == sub
 
 
-# Arguments:
-    # wav_folder
-    # amk_folder
-    # sample_subfolder
-# Calls: Converter
-# Output: ./tuning.yaml
+@contextmanager
+def is_moved(dir: Path, name, method: str, is_moved: bool) -> None:
+    before = dir / name
+    after = dir / wav2brr.BACKUP_ROOT / name
+
+    getattr(before, method)(exist_ok=True)
+    yield
+
+    assert after.exists() == is_moved
+    assert before.exists() != is_moved
+
+
+"""
+Arguments:
+- wav_folder
+- amk_folder
+- sample_subfolder
+Calls: Converter
+Output: ./tuning.yaml
+"""
+def test_sample_dir(filesystem_tree):
+    runner = CliRunner()
+
+    # Valid sample path
+    result = runner.invoke(wav2brr.main, [WAV, AMK, SAMPLE], catch_exceptions=False)
+    assert result.exit_code == 0
+    assert 'Creating sample folder' in result.output
+    assert Path(AMK, 'samples', SAMPLE).exists()
+
+    # Ensure existing files are backed up, not deleted
+    assert '/' not in wav2brr.BACKUP_ROOT
+    sample_dir = AMK_SAMPLES / SAMPLE
+
+    for i in range(2):  # Ensure backups are overwritten without errors
+        with is_moved(sample_dir, 'file', 'touch', True), \
+             is_moved(sample_dir, 'directory', 'mkdir', False):
+            result = runner.invoke(wav2brr.main, [WAV, AMK, SAMPLE], catch_exceptions=False)
+            assert result.exit_code == 0
+            assert 'Creating sample folder' not in result.output
+
+    # Invalid sample path (attempting to mangle sample-root folder)
+    with pytest.raises(click.BadParameter):
+        # click is weird.
+        # catch_exceptions=False stops the test runner from eating exceptions.
+        # standalone_mode=False stops main() from pretty-printing BadParameter.
+        runner.invoke(wav2brr.main, [WAV, AMK, '.'], catch_exceptions=False,
+                      standalone_mode=False)
+
+
 # def test_main():
 #     pass  # TODO
 
@@ -92,44 +135,3 @@ def test_cfg(cfg_name, expected):
         #     file.unlink()
 
     assert tuning == expected
-
-
-@contextmanager
-def is_moved(dir: Path, name, method: str, is_moved: bool) -> None:
-    before = dir / name
-    after = dir / wav2brr.BACKUP_ROOT / name
-
-    getattr(before, method)(exist_ok=True)
-    yield
-
-    assert after.exists() == is_moved
-    assert before.exists() != is_moved
-
-
-def test_sample_dir(filesystem_tree):
-    runner = CliRunner()
-
-    # Valid sample path
-    result = runner.invoke(wav2brr.main, [WAV, AMK, SAMPLE], catch_exceptions=False)
-    assert result.exit_code == 0
-    assert 'Creating sample folder' in result.output
-    assert Path(AMK, 'samples', SAMPLE).exists()
-
-    # Ensure existing files are backed up, not deleted
-    assert '/' not in wav2brr.BACKUP_ROOT
-    sample_dir = AMK_SAMPLES / SAMPLE
-
-    for i in range(2):  # Ensure backups are overwritten without errors
-        with is_moved(sample_dir, 'file', 'touch', True), \
-             is_moved(sample_dir, 'directory', 'mkdir', False):
-            result = runner.invoke(wav2brr.main, [WAV, AMK, SAMPLE], catch_exceptions=False)
-            assert result.exit_code == 0
-            assert 'Creating sample folder' not in result.output
-
-    # Invalid sample path (attempting to mangle sample-root folder)
-    with pytest.raises(click.BadParameter):
-        # click is weird.
-        # catch_exceptions=False stops the test runner from eating exceptions.
-        # standalone_mode=False stops main() from pretty-printing BadParameter.
-        runner.invoke(wav2brr.main, [WAV, AMK, '.'], catch_exceptions=False,
-                      standalone_mode=False)
