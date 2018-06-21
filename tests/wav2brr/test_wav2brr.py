76a13036d737b81b0c8e7876c28412602b059c6b
nyanpasu64 <nyanpasu64@tuta.io>
2018-06-20 22:54:24 -0700

    Add unit tests for resampling ratio

--- tests/wav2brr/test_wav2brr.py
+++ tests/wav2brr/test_wav2brr.py
@@ -1,6 +1,9 @@
+import itertools
+import tempfile
 from contextlib import contextmanager
 from os import mkdir, chdir as cd
 from pathlib import Path
+from unittest.mock import MagicMock, patch
 
 import click
 import pytest
@@ -14,6 +17,7 @@ SAMPLE = 'lol sample'
 
 AMK_SAMPLES = Path(AMK, 'samples')
 PROJECT = 'project'
+
 
 
 @pytest.fixture(scope='function')
@@ -50,6 +54,44 @@ def test_pushd():
         with wav2brr.pushd(Path(SUB)):
             sub = Path().resolve()
             assert root / SUB == sub
+
+
+# Arguments:
+    # wav_folder
+    # amk_folder
+    # sample_subfolder
+# Calls: Converter
+# Output: ./tuning.yaml
+# def test_main():
+#     pass  # TODO
+
+
+# Source: wav_folder
+# Destination: amk_folder/samples/sample_subfolder
+
+@pytest.mark.parametrize("cfg_name,expected", [
+    ('test.cfg', '$08 $00'),
+    ('test2.cfg', '$03 $80'),
+])
+def test_cfg(cfg_name, expected):
+    runner = CliRunner()
+
+    sample_output = tempfile.mkdtemp()
+
+    opt = wav2brr.CliOptions(
+        verbose=2,
+        sample_folder=sample_output,
+        decode_loops=1)
+
+    cfg_path = Path(__file__, '..', cfg_name).resolve()
+    try:
+        brr_path, tuning = wav2brr.convert_cfg(opt, str(cfg_path), {})
+    finally:
+        cfg_dir = cfg_path.parent.resolve()
+        # for file in itertools.chain(cfg_dir.glob('* decoded.wav'), cfg_dir.glob('*.brr')):
+        #     file.unlink()
+
+    assert tuning == expected
 
 
 @contextmanager
